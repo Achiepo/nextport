51e8c7d370bee1ceb46f8ab442837e25e5018b40
Achiepo <yvanachiepo@icloud.com>
2025-03-23 20:20:03 +0000

    mise a jour du ptojet

--- app/serveur/sign-up/route.ts
+++ app/serveur/sign-up/route.ts
@@ -15,7 +15,7 @@ export const POST = async (req: Request) => {
     console.log('Utilisateur ajouté à la base de données:', id)
     return NextResponse.json({data})
    } catch (error) {
-    console.log()
+    console.error(error)
     return NextResponse.json("Une erreur s'est produite")
    }
 }
